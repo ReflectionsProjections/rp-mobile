2166074a56bb7689813a6e09e6174104f549780d
Aditya Kshirsagar <aditya29@icloud.com>
2025-06-27 18:46:53 -0500

    initial point Shop functionings, just need to add question marks

--- app/(tabs)/points_shop.tsx
+++ app/(tabs)/points_shop.tsx
@@ -1,10 +1,36 @@
+// screens/PointsScreen.tsx
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Dimensions } from 'react-native';
+import Background from '../../assets/pointshop/point_background.svg';
+import { PointsGauge } from '@/components/pointshop/PointsGuage';
 
-const PointsShopScreen = () => (
-  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    <Text style={{ color: '#f0f0f0' }}>Points Shop Screen</Text>
-  </View>
-);
+const { width, height } = Dimensions.get('window');
+const SPEEDO_WIDTH = width * 0.7;
 
-export default PointsShopScreen;
+export default function PointsScreen() {
+  return (
+    <View className="flex-1 bg-rpRed relative">
+      <Background
+        width={width}
+        height={height}
+        className="absolute inset-0 z-0"
+        preserveAspectRatio="xMidYMin slice"
+      />
+
+      <View className="absolute inset-x-0 top-16 items-center z-10">
+        <PointsGauge points={20} width={SPEEDO_WIDTH} />
+      </View>
+
+      <Text
+        className="absolute z-10 text-[16px] font-bold text-black font-proRacing"
+        style={{
+          top: height * 0.70,
+          left: width * 0.65,
+          width: width * 0.35,
+        }}
+      >
+        Attend events to earn points and unlock prizes!
+      </Text>
+    </View>
+  );
+}
