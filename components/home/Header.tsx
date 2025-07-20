eb736becb89abf98b25d88b111c0aba83ba525b9
anandani4136 <ra7353@hotmail.com>
2025-07-20 00:38:14 -0500

    add logout confirmation

--- components/home/Header.tsx
+++ components/home/Header.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Image, TouchableOpacity } from 'react-native';
+import { View, Image, TouchableOpacity, Alert } from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
 import { router } from 'expo-router';
 import * as SecureStore from 'expo-secure-store';
@@ -11,10 +11,22 @@ export const Header: React.FC = () => {
     router.push('/');
   };
 
+  const confirmLogout = () => {
+    Alert.alert(
+      'Log Out',
+      'Are you sure you want to log out?',
+      [
+        { text: 'Cancel', style: 'cancel' },
+        { text: 'Log Out', style: 'destructive', onPress: handleLogout },
+      ],
+      { cancelable: true }
+    );
+  };
+
   return (
     <View className="flex-row p-4 justify-between items-center">
       <Image source={require('../../assets/images/rp-logo.png')} className="w-8 h-8" />
-      <TouchableOpacity onPress={() => handleLogout()}>
+      <TouchableOpacity onPress={confirmLogout}>
         <FontAwesome name="user-circle-o" size={28} color="#fff" />
       </TouchableOpacity>
     </View>
