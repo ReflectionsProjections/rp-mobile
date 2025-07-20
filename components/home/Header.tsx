c48899caadd9f25d240954f5d0278a5c318f9dc1
anandani4136 <ra7353@hotmail.com>
2025-07-20 10:47:14 -0500

    combine functionality

--- components/home/Header.tsx
+++ components/home/Header.tsx
@@ -5,19 +5,21 @@ import { router } from 'expo-router';
 import * as SecureStore from 'expo-secure-store';
 
 export const Header: React.FC = () => {
-  const handleLogout = async () => {
-    await SecureStore.deleteItemAsync('jwt');
-    await SecureStore.deleteItemAsync('codeVerifier');
-    router.push('/');
-  };
-
-  const confirmLogout = () => {
+  const handleLogout = () => {
     Alert.alert(
       'Log Out',
       'Are you sure you want to log out?',
       [
         { text: 'Cancel', style: 'cancel' },
-        { text: 'Log Out', style: 'destructive', onPress: handleLogout },
+        {
+          text: 'Log Out',
+          style: 'destructive',
+          onPress: async () => {
+            await SecureStore.deleteItemAsync('jwt');
+            await SecureStore.deleteItemAsync('codeVerifier');
+            router.push('/');
+          },
+        },
       ],
       { cancelable: true }
     );
@@ -26,7 +28,7 @@ export const Header: React.FC = () => {
   return (
     <View className="flex-row p-4 justify-between items-center">
       <Image source={require('../../assets/images/rp-logo.png')} className="w-8 h-8" />
-      <TouchableOpacity onPress={confirmLogout}>
+      <TouchableOpacity onPress={handleLogout}>
         <FontAwesome name="user-circle-o" size={28} color="#fff" />
       </TouchableOpacity>
     </View>
