324bbb7eebebb26df1b378b61fba4294aaba6bb4
Aryan-Bahl <abahl3@illinois.edu>
2025-08-21 12:24:14 -0500

    commented out notifications for now

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -11,7 +11,7 @@ import { Text } from 'react-native';
 import Toast from 'react-native-toast-message';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
-import { useFirebaseNotifications } from '@/hooks/useFirebaseNotifications';
+// import { useFirebaseNotifications } from '@/hooks/useFirebaseNotifications';
 
 SplashScreen.preventAutoHideAsync();
 
@@ -28,11 +28,11 @@ RNText.defaultProps = {
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
-  const {
-    fcmToken,
-    isLoading: notificationsLoading,
-    error: notificationsError,
-  } = useFirebaseNotifications();
+  // const {
+  //   fcmToken,
+  //   isLoading: notificationsLoading,
+  //   error: notificationsError,
+  // } = useFirebaseNotifications();
   const [loaded] = useFonts({
     RacingSansOne: require('../assets/fonts/RacingSansOne-Regular.ttf'),
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
